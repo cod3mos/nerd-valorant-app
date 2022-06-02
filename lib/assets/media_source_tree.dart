c75e75d5891a29cff1b8dd34e895b0130874ccec
cod3mos <contato@codemos.com.br>
2022-06-02 20:37:56 -0300

    feat: adicionado variáveis de origem para assets

---
+++ lib/assets/media_source_tree.dart
@@ -0,0 +1,5 @@
+// Assets: Onboarding
+String screenBackground = 'assets/images/onboarding/screen_background.png';
+String screenImageViper = 'assets/images/onboarding/screen_image_viper.png';
+String screenImageCypher = 'assets/images/onboarding/screen_image_cypher.png';
+String screenImageKilljoy = 'assets/images/onboarding/screen_image_killjoy.png';
