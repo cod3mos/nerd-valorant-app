5200f166eaba5344a754414867718dd7821532d2
cod3mos <contato@codemos.com.br>
2022-06-02 20:36:36 -0300

    style: renomeado o nome das variáveis das cores

--- lib/themes/global_styles.dart
+++ lib/themes/global_styles.dart
@@ -1,16 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:nerdvalorant/mobile/screen_size.dart';
 
-// --------------------------------------------------------------------------------------------------------------------------------
-// Adaptive Size: 2 => 8px | 2.5 => 10px | 3.5 => 14px | 4 => 16px  | 5 => 20px | 6 => 24px | 8 => 32px | 9 => 36px | 10 => 40px
-// --------------------------------------------------------------------------------------------------------------------------------
+// ------------------------------------------------------------------------------------------------------------------------------------------
+// Adaptive Size: 2 => 8px | 2.5 => 10px | 3.5 => 14px | 4 => 16px  | 5 => 20px | 6 => 24px | 7 => 28px | 8 => 32px | 9 => 36px | 10 => 40px
+// ------------------------------------------------------------------------------------------------------------------------------------------
 
-Color primaryColor = const Color.fromRGBO(242, 244, 246, 1);
-Color secondaryColor = const Color.fromRGBO(22, 23, 27, 1);
-Color tertiaryColor = const Color.fromRGBO(25, 195, 98, 1);
+Color whiteColor = const Color.fromRGBO(242, 244, 246, 1);
+Color blackColor = const Color.fromRGBO(22, 23, 27, 1);
+Color greenColor = const Color.fromRGBO(25, 195, 98, 1);
 
 final textStyle = TextStyle(
-  color: secondaryColor,
+  color: blackColor,
   fontFamily: 'Roboto',
   fontWeight: FontWeight.normal,
   fontSize: ScreenSize.adaptiveFontSize(4),
