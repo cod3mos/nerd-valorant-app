45cc1be1cb29dea2c64dcb168fb5695bff1f828a
cod3mos <contato@codemos.com.br>
2022-06-02 23:45:32 -0300

    feat: adicionado icones da tela de onboarding

--- lib/assets/media_source_tree.dart
+++ lib/assets/media_source_tree.dart
@@ -1,4 +1,13 @@
 // Assets: Onboarding
+String iconMap = 'assets/icons/icon_map.svg';
+String iconAlbums = 'assets/icons/icon_albums.svg';
+String iconPerson = 'assets/icons/icon_person.svg';
+String iconTrophy = 'assets/icons/icon_trophy.svg';
+String iconInstagram = 'assets/icons/icon_instagram.svg';
+String iconNerdValorant = 'assets/icons/icon_nerd_valorant.svg';
+String iconArrowForward = 'assets/icons/icon_arrow_forward.svg';
+String iconShieldAndSword = 'assets/icons/icon_shield_and_sword.svg';
+String iconChatboxEllipses = 'assets/icons/icon_chatbox_ellipses.svg';
 String screenBackground = 'assets/images/onboarding/screen_background.png';
 String screenImageViper = 'assets/images/onboarding/screen_image_viper.png';
 String screenImageCypher = 'assets/images/onboarding/screen_image_cypher.png';
