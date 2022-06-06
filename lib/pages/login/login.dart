b7c63e01a25c3c5551886b9b648dc882ed2c5969
cod3mos <contato@codemos.com.br>
2022-06-06 17:10:21 -0300

    feat: adicionado layout da tela de login

--- lib/pages/login/login.dart
+++ lib/pages/login/login.dart
@@ -1,43 +1,116 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:nerdvalorant/keys/version.dart';
 import 'package:nerdvalorant/mobile/screen_size.dart';
+import 'package:nerdvalorant/pages/login/styles.dart';
 import 'package:nerdvalorant/themes/global_styles.dart';
+import 'package:nerdvalorant/assets/media_source_tree.dart';
 
 class LoginPage extends StatelessWidget {
   const LoginPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    // final arguments = ModalRoute.of(context)?.settings.arguments ?? 'Login';
-
     ScreenSize.init(context);
+
+    print(ScreenSize.height(8));
 
     return SafeArea(
       child: Scaffold(
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                'Login',
-                style: TextStyle(
-                  color: blackColor,
-                  fontSize: 30,
+        body: Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage(screenBackground),
+              fit: BoxFit.cover,
+            ),
+          ),
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Image.asset(
+                  logoNerdValorant,
+                  width: ScreenSize.width(60),
+                  height: ScreenSize.screenHeight * .65,
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(top: 20),
-                child: TextButton(
-                  style: ElevatedButton.styleFrom(
-                    primary: blackColor,
+                SizedBox(
+                  width: 280,
+                  height: 48,
+                  child: TextButton(
+                    style: ButtonStyle(
+                      shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                        RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(5.0),
+                          side: BorderSide(
+                            color: whiteColor,
+                          ),
+                        ),
+                      ),
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        SizedBox(
+                          width: ScreenSize.width(7),
+                          height: ScreenSize.width(7),
+                          child: SvgPicture.asset(iconGoogle),
+                        ),
+                        SizedBox(
+                          width: ScreenSize.width(5),
+                        ),
+                        Text(
+                          'Entrar com o google',
+                          style: googleButtonStyle,
+                        ),
+                      ],
+                    ),
+                    onPressed: () =>
+                        Navigator.pushNamed(context, '/onboarding'),
                   ),
+                ),
+                SizedBox(
+                  width: ScreenSize.screenWidth * .8,
+                  child: RichText(
+                    textAlign: TextAlign.center,
+                    text: TextSpan(
+                      children: [
+                        TextSpan(
+                          text:
+                              'Ao tocar em entrar, você concorda com os nossos ',
+                          style: termStyle,
+                        ),
+                        TextSpan(
+                          text: 'Termos',
+                          style: linkStyle,
+                        ),
+                        TextSpan(
+                          text:
+                              '. Saiba como processamos seus dados em nossa  ',
+                          style: termStyle,
+                        ),
+                        TextSpan(
+                          text: 'Política de Privacidade',
+                          style: linkStyle,
+                        ),
+                        TextSpan(
+                          text: '.',
+                          style: termStyle,
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(bottom: ScreenSize.height(1)),
                   child: Text(
-                    'Voltar para tela de introdução',
-                    style: TextStyle(color: whiteColor),
+                    'Versão $version',
+                    style: TextStyle(
+                      color: whiteColor,
+                    ),
                   ),
-                  onPressed: () => Navigator.pushNamed(context, '/onboarding'),
                 ),
-              )
-            ],
+              ],
+            ),
           ),
         ),
       ),
---
+++ lib/pages/login/styles.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:nerdvalorant/mobile/screen_size.dart';
+import 'package:nerdvalorant/themes/global_styles.dart';
+
+final termStyle = TextStyle(
+  color: whiteColor,
+  fontFamily: 'Roboto',
+  fontWeight: FontWeight.w500,
+  fontSize: ScreenSize.adaptiveFontSize(3),
+);
+
+final linkStyle = TextStyle(
+  color: greenColor,
+  fontFamily: 'Roboto',
+  fontWeight: FontWeight.w500,
+  fontSize: ScreenSize.adaptiveFontSize(3),
+);
+
+final googleButtonStyle = TextStyle(
+  color: whiteColor,
+  fontFamily: 'Roboto',
+  fontWeight: FontWeight.w500,
+  fontSize: ScreenSize.adaptiveFontSize(4),
+);
