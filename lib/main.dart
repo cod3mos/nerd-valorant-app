6fd92f1a0937dd086adfaf4938b77b04584ed87d
cod3mos <contato@codemos.com.br>
2022-06-09 15:56:20 -0300

    refactor: alterado main para validar a página e alterado as cores da statusbar

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:provider/provider.dart';
 import 'package:nerdvalorant/routes/routes.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:nerdvalorant/pages/login/login.dart';
 import 'package:nerdvalorant/mobile/local_storage.dart';
+import 'package:nerdvalorant/themes/global_styles.dart';
+import 'package:nerdvalorant/services/google_sign_in.dart';
+import 'package:nerdvalorant/pages/dashboard/dashboard.dart';
+import 'package:nerdvalorant/pages/onboarding/onboarding.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  await LocalStorage.init();
+  await Future.wait([LocalStorage.init(), Firebase.initializeApp()]);
 
-  runApp(const NerdValorantApp());
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (context) => GoogleSignInProvider(),
+        ),
+      ],
+      child: const NerdValorantApp(),
+    ),
+  );
 }
 
 class NerdValorantApp extends StatelessWidget {
@@ -15,15 +32,55 @@ class NerdValorantApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(
+      SystemUiOverlayStyle(
+        statusBarColor: appBarColor,
+        statusBarIconBrightness: Brightness.light,
+        systemNavigationBarColor: appBarColor,
+        systemNavigationBarIconBrightness: Brightness.light,
+      ),
+    );
+
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      initialRoute: initialRoute(),
       routes: Routes.list,
+      initialRoute: Routes.initialRoute,
       navigatorKey: Routes.navigatorKey,
     );
   }
+}
 
-  String initialRoute() {
-    return LocalStorage.readBool('seenIntro') ? '/login' : '/onboarding';
+class VerifyAuth extends StatefulWidget {
+  const VerifyAuth({Key? key}) : super(key: key);
+
+  @override
+  State<VerifyAuth> createState() => _VerifyAuthState();
+}
+
+class _VerifyAuthState extends State<VerifyAuth> {
+  late bool alreadyViewed;
+
+  @override
+  void initState() {
+    super.initState();
+
+    setState(() => alreadyViewed = LocalStorage.readBool('seenIntro'));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    GoogleSignInProvider auth = Provider.of<GoogleSignInProvider>(context);
+
+    if (auth.isLoading) {
+      return const Scaffold(
+        body: Center(
+          child: CircularProgressIndicator(),
+        ),
+      );
+    } else if (auth.googleUser == null) {
+      return alreadyViewed ? const LoginPage() : const OnboardingPage();
+    } else {
+      return const DashboardPage();
+    }
   }
 }
