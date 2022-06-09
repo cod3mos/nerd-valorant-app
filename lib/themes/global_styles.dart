bc03af78208bbce0fb2ce7c0ad9318183e742216
cod3mos <contato@codemos.com.br>
2022-06-09 15:52:30 -0300

    style: adicionado nova cor a paleta global

--- lib/themes/global_styles.dart
+++ lib/themes/global_styles.dart
@@ -3,3 +3,5 @@ import 'package:flutter/material.dart';
 Color whiteColor = const Color.fromRGBO(242, 244, 246, 1);
 Color blackColor = const Color.fromRGBO(22, 23, 27, 1);
 Color greenColor = const Color.fromRGBO(25, 195, 98, 1);
+
+Color appBarColor = const Color.fromRGBO(10, 10, 10, 1);
