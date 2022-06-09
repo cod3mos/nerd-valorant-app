922acefe657a8a924d62141a5b19640980c70b18
cod3mos <contato@codemos.com.br>
2022-06-09 15:53:52 -0300

    refactor: centralizado escolha de rotas no verifyAuth

--- lib/routes/routes.dart
+++ lib/routes/routes.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/material.dart';
-import 'package:nerdvalorant/pages/login/login.dart';
-import 'package:nerdvalorant/pages/onboarding/onboarding.dart';
+import 'package:nerdvalorant/main.dart';
 
 class Routes {
   static Map<String, Widget Function(BuildContext)> list =
       <String, WidgetBuilder>{
-    '/onboarding': (context) => const OnboardingPage(),
-    '/login': (context) => const LoginPage(),
+    '/verify_auth': (context) => const VerifyAuth(),
   };
+
+  static String initialRoute = '/verify_auth';
 
   static GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
 }
