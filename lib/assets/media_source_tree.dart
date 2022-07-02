8e70d0adec36bcce890356c5836784de0ca70985
cod3mos <contato@codemos.com.br>
2022-07-02 13:52:05 -0300

    feat: adicionado novo pack de imagens para o app

--- lib/assets/media_source_tree.dart
+++ lib/assets/media_source_tree.dart
@@ -1,14 +1,17 @@
 // Icons
 String iconMap = 'assets/icons/icon_map.svg';
+String iconSword = 'assets/icons/icon_sword.svg';
 String iconGoogle = 'assets/icons/icon_google.svg';
 String iconAlbums = 'assets/icons/icon_albums.svg';
 String iconPerson = 'assets/icons/icon_person.svg';
 String iconTrophy = 'assets/icons/icon_trophy.svg';
 String iconInstagram = 'assets/icons/icon_instagram.svg';
+String iconNotification = 'assets/icons/icon_notification.svg';
 String iconNerdValorant = 'assets/icons/icon_nerd_valorant.svg';
 String iconArrowForward = 'assets/icons/icon_arrow_forward.svg';
 String iconShieldAndSword = 'assets/icons/icon_shield_and_sword.svg';
 String iconChatboxEllipses = 'assets/icons/icon_chatbox_ellipses.svg';
+String iconNotificationAlert = 'assets/icons/icon_notification_alert.svg';
 
 // Onboarding
 String screenBackground = 'assets/images/onboarding/screen_background.png';
@@ -17,7 +20,9 @@ String screenImageCypher = 'assets/images/onboarding/screen_image_cypher.png';
 String screenImageKilljoy = 'assets/images/onboarding/screen_image_killjoy.png';
 
 // Shared
+String bannerBackground = 'assets/images/shared/banner_background.png';
 String logoNerdValorant = 'assets/images/shared/logo_nerd_valorant.png';
+String signUpBackground = 'assets/images/shared/sign_up_background.png';
 
 // Stickers
 String stickerBrim = 'assets/images/stickers/agent_brim.png';
@@ -27,3 +32,33 @@ String stickerSova = 'assets/images/stickers/agent_sova.png';
 String stickerViper = 'assets/images/stickers/agent_viper.png';
 String stickerCypher = 'assets/images/stickers/agent_cypher.png';
 String stickerKilljoy = 'assets/images/stickers/agent_killjoy.png';
+
+// App
+String appNerdValorant = 'assets/app/icon_foreground.png';
+
+// Profiles
+String agentJet = 'assets/images/profiles/jet.png';
+String agentFade = 'assets/images/profiles/fade.png';
+String agentKayo = 'assets/images/profiles/kayo.png';
+String agentNeon = 'assets/images/profiles/neon.png';
+String agentOmen = 'assets/images/profiles/omen.png';
+String agentRaze = 'assets/images/profiles/raze.png';
+String agentSage = 'assets/images/profiles/sage.png';
+String agentSova = 'assets/images/profiles/sova.png';
+String agentYoru = 'assets/images/profiles/yoru.png';
+String agentAstra = 'assets/images/profiles/astra.png';
+String agentReyna = 'assets/images/profiles/reyna.png';
+String agentViper = 'assets/images/profiles/viper.png';
+String agentFenix = 'assets/images/profiles/fenix.png';
+String agentCypher = 'assets/images/profiles/cypher.png';
+String agentKilljoy = 'assets/images/profiles/killjoy.png';
+String agentChamber = 'assets/images/profiles/chamber.png';
+String agentBrimstone = 'assets/images/profiles/brimstone.png';
+
+// Maps
+String mapBind = 'assets/images/maps/bind.png';
+String mapSplit = 'assets/images/maps/split.png';
+String mapAscent = 'assets/images/maps/ascent.png';
+String mapBreeze = 'assets/images/maps/breeze.png';
+String mapIceBox = 'assets/images/maps/icebox.png';
+String mapFracture = 'assets/images/maps/fracture.png';
